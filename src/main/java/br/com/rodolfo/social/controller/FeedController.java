1291bca206d51295fb9f1424a689038f56c3dc81
Rodolfo Infantini Carneiro <rodolfo.carneiro@sensedia.com>
2022-04-11 15:26:03 -0300

    dtos on feed controller

--- src/main/java/br/com/rodolfo/social/controller/FeedController.java
+++ src/main/java/br/com/rodolfo/social/controller/FeedController.java
@@ -1,8 +1,8 @@
 package br.com.rodolfo.social.controller;
 
+import br.com.rodolfo.social.dto.PostDto;
 import br.com.rodolfo.social.exception.ForbiddenException;
 import br.com.rodolfo.social.exception.NotFoundException;
-import br.com.rodolfo.social.model.Post;
 import br.com.rodolfo.social.service.FeedService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +20,7 @@ public class FeedController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Post> getFeed(@RequestHeader("Authorization") String token) throws ForbiddenException, NotFoundException {
-        return feedService.getFeed(token);
+    public List<PostDto> getFeed(@RequestHeader("Authorization") String token) throws ForbiddenException, NotFoundException {
+        return PostDto.from(feedService.getFeed(token));
     }
 }
